3a04b79a4caeb9ee4820d8ff087e70a447fef7ff
Martin Tripode <mtripode@gmail.com>
2025-12-31 14:32:53 -0300

    saving company pages fe

--- src/pages/CompanyForm.tsx
+++ src/pages/CompanyForm.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createCompany } from "../services/companyService";
+import { CompanyDto } from "../types/company";
 
 export default function CompanyForm() {
-  const [name, setName] = useState("");
-  const [website, setWebsite] = useState("");
+  const [name, setName] = useState<string>("");
+  const [website, setWebsite] = useState<string>("");
+  const [description, setDescription] = useState<string>(""); // opcional
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    createCompany({
+
+    const newCompany: CompanyDto = {
       name,
       website,
-    })
+      description,
+    };
+
+    createCompany(newCompany)
       .then(() => {
         // redirect to list after successful creation
         navigate("/companies");
@@ -26,6 +32,7 @@ export default function CompanyForm() {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Add Company</h2>
+
       <input
         type="text"
         placeholder="Company name"
@@ -33,13 +40,21 @@ export default function CompanyForm() {
         onChange={(e) => setName(e.target.value)}
         required
       />
+
       <input
         type="url"
         placeholder="Website"
         value={website}
         onChange={(e) => setWebsite(e.target.value)}
       />
-       <button type="submit">Save</button>
+
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button type="submit">Save</button>
     </form>
   );
 }
